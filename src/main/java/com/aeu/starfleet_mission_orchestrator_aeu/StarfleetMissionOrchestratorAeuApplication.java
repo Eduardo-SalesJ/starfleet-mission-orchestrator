a3ab346cb62474fccef6d37e0ccbaa5ba0fbe99e
E <eduardo-jr12@hotmail.com>
2025-06-28 17:28:57 -0300

    Iniciando a estrutura do projeto

---
+++ src/main/java/com/aeu/starfleet_mission_orchestrator_aeu/StarfleetMissionOrchestratorAeuApplication.java
@@ -0,0 +1,13 @@
+package com.aeu.starfleet_mission_orchestrator_aeu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StarfleetMissionOrchestratorAeuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StarfleetMissionOrchestratorAeuApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/aeu/starfleet_mission_orchestrator_aeu/StarfleetMissionOrchestratorAeuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aeu.starfleet_mission_orchestrator_aeu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StarfleetMissionOrchestratorAeuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
