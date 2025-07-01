d6221ca5ac3ccdbca7c5fa8a27e2cb8855d36505
E <eduardo-jr12@hotmail.com>
2025-07-01 00:48:02 -0300

    incluindo regras para criar ou atualizar membros da frota.

--- src/main/java/com/aeu/starfleet_mission_orchestrator_aeu/dto/request/FleetMemberRequestDto.java
+++ src/main/java/com/aeu/starfleet_mission_orchestrator_aeu/dto/request/FleetMemberRequestDto.java
@@ -2,14 +2,26 @@ package com.aeu.starfleet_mission_orchestrator_aeu.dto.request;
 
 import com.aeu.starfleet_mission_orchestrator_aeu.model.enums.Rank;
 import com.aeu.starfleet_mission_orchestrator_aeu.model.enums.Specialty;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 @Data
 public class FleetMemberRequestDto {
+    @NotBlank
+    @Size(min = 2, max = 100)
     private String name;
+    @Size(min = 4,max = 40, message = "The username must be between 4 and 40 characters long")
     private String username;
+    @NotBlank
+    @Size(min = 8, message = "Password must be at least 6 characters long")
     private String password;
+    @NotNull
     private Rank ranks;
+    @NotBlank
     private Specialty specialty;
+    @Min(value = 0)
     private int experience;
 }
