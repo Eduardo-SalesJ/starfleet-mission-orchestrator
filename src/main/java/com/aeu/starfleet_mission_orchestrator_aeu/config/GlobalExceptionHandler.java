ea823a804ac0371a215ee2b902af9e885577562e
E <eduardo-jr12@hotmail.com>
2025-07-03 21:44:49 -0300

    Implementando mais configurações no retorno Http 404.

--- src/main/java/com/aeu/starfleet_mission_orchestrator_aeu/config/GlobalExceptionHandler.java
+++ src/main/java/com/aeu/starfleet_mission_orchestrator_aeu/config/GlobalExceptionHandler.java
@@ -1,19 +1,23 @@
 package com.aeu.starfleet_mission_orchestrator_aeu.config;
 
+import com.aeu.starfleet_mission_orchestrator_aeu.dto.error.ErrorResponse;
 import com.aeu.starfleet_mission_orchestrator_aeu.exception.ResourceNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.ErrorResponse;
 import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import java.time.LocalDateTime;
 
+
 @ControllerAdvice
 public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
-    // Tratamento para HTTP 404 NOT FOUND
-    public ResponseEntity<ErrorResponse> handleResourceNotFoundException(ResourceNotFoundException ex, WebRequest request){
+
+    // 1. Tratamento para ResourceNotFoundException (HTTP 404 Not Found)
+    @ExceptionHandler(ResourceNotFoundException.class)
+    public ResponseEntity<ErrorResponse> handleResourceNotFoundException(ResourceNotFoundException ex, WebRequest request) {
         ErrorResponse errorResponse = new ErrorResponse(
                 LocalDateTime.now(),
                 HttpStatus.NOT_FOUND.value(),
@@ -22,5 +26,6 @@ public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
                 request.getDescription(false),
                 null
         );
+        return new ResponseEntity<>(errorResponse, HttpStatus.NOT_FOUND);
     }
 }
--- src/main/java/com/aeu/starfleet_mission_orchestrator_aeu/exception/ResourceNotFoundException.java
+++ src/main/java/com/aeu/starfleet_mission_orchestrator_aeu/exception/ResourceNotFoundException.java
@@ -3,12 +3,14 @@ package com.aeu.starfleet_mission_orchestrator_aeu.exception;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-@ResponseStatus(HttpStatus.NOT_FOUND)
-public class ResourceNotFoundException {
-    public ResourceNotFoundException(String message){
-        super();
+@ResponseStatus(HttpStatus.NOT_FOUND) // Define o status HTTP padrão para esta exceção
+public class ResourceNotFoundException extends RuntimeException {
+
+    public ResourceNotFoundException(String message) {
+        super(message);
     }
-    public ResourceNotFoundException(String message, Throwable cause){
-        super();
+    public ResourceNotFoundException(String message, Throwable cause) {
+        super(message, cause);
     }
 }
+
