ab06c41c6d702d329b11f4a9834248f2c24b15cc
E <eduardo-jr12@hotmail.com>
2025-07-03 21:47:22 -0300

    Implementando o tratamento de validação dos Dtos.

--- src/main/java/com/aeu/starfleet_mission_orchestrator_aeu/config/GlobalExceptionHandler.java
+++ src/main/java/com/aeu/starfleet_mission_orchestrator_aeu/config/GlobalExceptionHandler.java
@@ -2,20 +2,26 @@ package com.aeu.starfleet_mission_orchestrator_aeu.config;
 
 import com.aeu.starfleet_mission_orchestrator_aeu.dto.error.ErrorResponse;
 import com.aeu.starfleet_mission_orchestrator_aeu.exception.ResourceNotFoundException;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import java.time.LocalDateTime;
+import java.util.HashMap;
+import java.util.Map;
 
 
 @ControllerAdvice
 public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
 
-    // 1. Tratamento para ResourceNotFoundException (HTTP 404 Not Found)
+    //Tratamento para ResourceNotFoundException (HTTP 404 Not Found)
     @ExceptionHandler(ResourceNotFoundException.class)
     public ResponseEntity<ErrorResponse> handleResourceNotFoundException(ResourceNotFoundException ex, WebRequest request) {
         ErrorResponse errorResponse = new ErrorResponse(
@@ -27,5 +33,27 @@ public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
                 null
         );
         return new ResponseEntity<>(errorResponse, HttpStatus.NOT_FOUND);
+    }
+    //Tratamento para validação dos DTOs
+    @Override
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(
+            MethodArgumentNotValidException ex, HttpHeaders headers, HttpStatusCode status, WebRequest request) {
+
+        Map<String, String> errors = new HashMap<>();
+        ex.getBindingResult().getAllErrors().forEach(error -> {
+            String fieldName = ((FieldError) error).getField();
+            String errorMessage = error.getDefaultMessage();
+            errors.put(fieldName, errorMessage);
+        });
+
+        ErrorResponse errorResponse = new ErrorResponse(
+                LocalDateTime.now(),
+                HttpStatus.BAD_REQUEST.value(),
+                "Validation Error",
+                "Validation errors in the fields provided.",
+                request.getDescription(false),
+                errors
+        );
+        return new ResponseEntity<>(errorResponse, HttpStatus.BAD_REQUEST);
     }
 }
