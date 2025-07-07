9793d9af236578abd23e47bb0614b09ce557469f
E <eduardo-jr12@hotmail.com>
2025-07-06 21:17:14 -0300

    Criando o endpoint POST para criar um novo membro.

--- src/main/java/com/aeu/starfleet_mission_orchestrator_aeu/controller/FleetMemberController.java
+++ src/main/java/com/aeu/starfleet_mission_orchestrator_aeu/controller/FleetMemberController.java
@@ -1,7 +1,14 @@
 package com.aeu.starfleet_mission_orchestrator_aeu.controller;
 
+import com.aeu.starfleet_mission_orchestrator_aeu.dto.request.FleetMemberRequestDto;
+import com.aeu.starfleet_mission_orchestrator_aeu.dto.response.FleetMemberResponseDto;
 import com.aeu.starfleet_mission_orchestrator_aeu.service.FleetMemberService;
+import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -12,5 +19,9 @@ public class FleetMemberController {
     public FleetMemberController(FleetMemberService fleetMemberService) {
         this.fleetMemberService = fleetMemberService;
     }
-
+    @PostMapping
+    public ResponseEntity<FleetMemberResponseDto> createFleetMember(@Valid FleetMemberRequestDto requestDto){
+        FleetMemberResponseDto response = fleetMemberService.createFleetMember(requestDto);
+                return new ResponseEntity<>(response, HttpStatus.CREATED);
+    }
 }
