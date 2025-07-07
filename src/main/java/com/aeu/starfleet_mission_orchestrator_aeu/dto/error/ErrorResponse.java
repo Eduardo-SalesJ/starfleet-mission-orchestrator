9da7f9312dafc636abe3ca8ac3be2b242d40aab8
E <eduardo-jr12@hotmail.com>
2025-07-06 21:40:49 -0300

    atualizando o lombok no pom.xml, inserindo o contrutor manualmente para o ErrorResponse

--- src/main/java/com/aeu/starfleet_mission_orchestrator_aeu/dto/error/ErrorResponse.java
+++ src/main/java/com/aeu/starfleet_mission_orchestrator_aeu/dto/error/ErrorResponse.java
@@ -1,15 +1,10 @@
 package com.aeu.starfleet_mission_orchestrator_aeu.dto.error;
 
-import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.NoArgsConstructor;
-
 import java.time.LocalDateTime;
 import java.util.Map;
 
 @Data
-@NoArgsConstructor
-@AllArgsConstructor
 public class ErrorResponse {
     private LocalDateTime timestamp;
     private int status;
@@ -18,4 +13,12 @@ public class ErrorResponse {
     private String path;
     private Map<String, String> validation;
 
+    public ErrorResponse(LocalDateTime timestamp, int status, String error, String message, String path, Map<String, String> validation) {
+        this.timestamp = timestamp;
+        this.status = status;
+        this.error = error;
+        this.message = message;
+        this.path = path;
+        this.validation = validation;
+    }
 }
